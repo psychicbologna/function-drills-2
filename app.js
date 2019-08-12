42242aba3682c57170c8fdbc09e2f143138c3104
Hubert Yang <offspring7890@yahoo.com>
2019-08-12 16:39:29 -0700

    add beyond switch/case function

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ function jediName(firstName, lastName) {
 }
 
 console.log(jediName('Carrie', 'Fisher'))
+
+function beyond(num) {
+    switch (true) {
+        case num === Infinity:
+            console.log('And beyond');
+            break;
+        case Number.isFinite(num) && num > 0:
+            console.log('To infinity');
+            break;
+        case Number.isFinite(num) && num < 0:
+            console.log('To negative infinity');
+            break;
+        case num === 0:
+            console.log('Staying home');
+            break;
+        default:
+        console.log('All done');
+    }
+}
+
+console.log(beyond(0));
