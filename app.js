e71770483f719cbceaa0b78c42b4628b4bb9464d
Alexander Fukui <alexander.fukui@gmail.com>
2019-08-12 18:11:29 -0500

    really done with it now

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-jediName(firstName lastName) {}
+'use strict'
+
+function jediName(firstName, lastName) {
+    return lastName.slice(0, 3) + firstName.slice(0, 2);
+}
+
+console.log(jediName('Carrie', 'Fisher'))
